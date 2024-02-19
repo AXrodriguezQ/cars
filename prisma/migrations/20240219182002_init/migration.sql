3e1b0da6fecae471efe5d36686ac54afed5f4596
AXrodriguezQ <160079823+AXrodriguezQ@users.noreply.github.com>
2024-02-19 13:28:18 -0500

    finish

--- prisma/migrations/20240219182002_init/migration.sql
+++ prisma/migrations/20240219182002_init/migration.sql
@@ -1,10 +1,12 @@
 -- CreateTable
 CREATE TABLE "Coches" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "nombre" TEXT NOT NULL,
     "precio" INTEGER NOT NULL,
     "info" TEXT NOT NULL,
     "imagen1" TEXT NOT NULL,
     "imagen2" TEXT,
-    "imagen3" TEXT
+    "imagen3" TEXT,
+
+    CONSTRAINT "Coches_pkey" PRIMARY KEY ("id")
 );
